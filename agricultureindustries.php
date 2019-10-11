ef4ba5bae684e354497247f6015344162d412317
Developeradmin92 <56434907+Developeradmin92@users.noreply.github.com>
2019-10-11 16:18:50 +0530

    Add files via upload

---
+++ agricultureindustries.php
@@ -0,0 +1,90 @@
+<?php
+include("include/stylesheet.php");
+include("include/header.php");
+?>
+<div class="innerbanner">
+	<img src="images/industries_bg.jpg">
+	<div class="innerbanner_content">
+		<h1>INDUSTRIES WE SERVE</h1>
+		<p>Precision.. Reliability.. Economy..</p>
+	</div>
+</div>
+<div class="breadcrumb">
+	<div class="container">
+			<ul>
+				<li><a href="./">Home</a></li>
+				<li><a href="./">/</a></li>
+				<li><a href="applications.php">industries we serve</a></li>
+				<li><a href="#">/</a></li>
+				<li>Agriculture & Cattle Feed Industry</li>
+			</ul>
+	</div>
+</div>
+<div class="innerpages_section">
+	<div class="chemical_bg">
+		<div class="container">
+		<div class="row clearfix">
+			<div class="col-lg-9 col-sm-12 col-xs-12">
+			<div class="chemical_industries agri">
+				<div class="chemical">
+					<img src="images/Industry/2.png">
+					<h2>Agriculture & Cattle Feed Industry</h2>
+					<!--<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release </p>-->
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-3 col-sm-12 col-xs-12">
+						<?php
+							include("include/sidebar.php");
+						?>
+		</div>
+	</div>
+	<div class="products_section">
+		<div class="container">
+			<h2>products</h2>
+			<div class="products_list">
+				<div class="row clearfix">
+					<div class="col-lg-3 col-sm-6 col-xs-12 products_desc_list">
+						<div class="products_content_list">
+							<a href="#">							
+								<img src="images/products/placeholder-image.jpg">
+								<h3>Lorem Ipsum </h3>
+							</a>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6 col-xs-12 products_desc_list">
+						<div class="products_content_list">
+							<a href="#">							
+								<img src="images/products/placeholder-image.jpg">
+								<h3>Lorem Ipsum </h3>
+							</a>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6 col-xs-12 products_desc_list">
+						<div class="products_content_list">
+							<a href="#">							
+								<img src="images/products/placeholder-image.jpg">
+								<h3>Lorem Ipsum </h3>
+							</a>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6 col-xs-12 products_desc_list">
+						<div class="products_content_list">
+							<a href="#">							
+								<img src="images/products/placeholder-image.jpg">
+								<h3>Lorem Ipsum </h3>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+</div>
+<?php
+include("include/calltoaction.php");
+include("include/footer.php");
+?>
+</div>
